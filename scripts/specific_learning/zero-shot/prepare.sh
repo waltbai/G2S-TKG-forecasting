b02a2ed70c24cc98acf775f183724ee61c046470
waltbai <bailong@ict.ac.cn>
2025-02-18 15:26:31 +0000

    [feat] update configs

--- scripts/specific_learning/zero-shot/prepare.sh
+++ scripts/specific_learning/zero-shot/prepare.sh
@@ -1,8 +1,8 @@
 # Prepare FID data
-python -m src.prepare config/specific_learning/zero-shot/prepare-FID.yaml
+python -m src.prepare config/specific_learning/zero-shot/prepare/FID.yaml
 
 # Prepare GID data
-python -m src.prepare config/specific_learning/zero-shot/prepare-GID.yaml
+python -m src.prepare config/specific_learning/zero-shot/prepare/GID.yaml
 
 # Prepare RID data
-python -m src.prepare config/specific_learning/zero-shot/prepare-RID.yaml
+python -m src.prepare config/specific_learning/zero-shot/prepare/RID.yaml
